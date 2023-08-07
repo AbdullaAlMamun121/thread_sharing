9de99fafb9e963a57aa65046daa1ab0260630b41
AbdullaAlMamun121 <hmdabdulla121@gmail.com>
2023-08-07 13:28:38 +0600

    approximately end the project

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,30 +1,47 @@
 "use server";
 
+import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
-import User from "../models/user.model";
-import { connectToDB } from "../mongoose";
+
+import Community from "../models/community.model";
 import Thread from "../models/thread.model";
-import { SortOrder } from "mongoose";
-import { FilterQuery } from "mongoose";
-interface Params{
-    userId:string;
-    username:string;
-    name:string;
-    bio:string;
-    image:string;
-    path:string;
+import User from "../models/user.model";
+
+import { connectToDB } from "../mongoose";
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+
+    return await User.findOne({ id: userId }).populate({
+      path: "communities",
+      model: Community,
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
 }
 
 export async function updateUser({
-    userId,
-    username,
-    name,
-    bio,
-    image,
-    path,
-}:Params): Promise<void> {
-  connectToDB();
+  userId,
+  bio,
+  name,
+  path,
+  username,
+  image,
+}: Params): Promise<void> {
   try {
+    connectToDB();
+
     await User.findOneAndUpdate(
       { id: userId },
       {
@@ -36,26 +53,13 @@ export async function updateUser({
       },
       { upsert: true }
     );
+
     if (path === "/profile/edit") {
       revalidatePath(path);
     }
-  } catch (error:any) {
-    throw new Error(`Failed to create/update user: ${error.message}`)
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
   }
-}
-
-export async function fetchUser(userId: string){
-    try{
-        connectToDB();
-        return await User
-        .findOne({id:userId})
-        // .populate({
-        //     path:'community',
-        //     model: Community
-        // })
-    }catch(error: any){
-        throw new Error(`Failed to fetch user: ${error.message}`)
-    }
 }
 
 export async function fetchUserPosts(userId: string) {
@@ -67,11 +71,11 @@ export async function fetchUserPosts(userId: string) {
       path: "threads",
       model: Thread,
       populate: [
-        // {
-        //   path: "community",
-        //   model: Community,
-        //   select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
-        // },
+        {
+          path: "community",
+          model: Community,
+          select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+        },
         {
           path: "children",
           model: Thread,
@@ -84,12 +88,13 @@ export async function fetchUserPosts(userId: string) {
       ],
     });
     return threads;
-  } catch (error:any) {
+  } catch (error) {
     console.error("Error fetching user threads:", error);
     throw error;
   }
 }
 
+// Almost similar to Thead (search + pagination) and Community (search + pagination)
 export async function fetchUsers({
   userId,
   searchString = "",
